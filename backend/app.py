449a41812d1cbef96cd975abc4f9f1e3dd34efd5
amulyaprasanth <amulyaprasanth301@gmail.com>
2025-11-02 11:01:52 +0530

    changed api link to prod

--- backend/app.py
+++ backend/app.py
@@ -101,7 +101,7 @@ app.include_router(router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                   "https://biblechatbot.netlify.app/"],
+                   "https://biblechatbot.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- versechat/src/api/axios.ts
+++ versechat/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://bible-chatbot-backend.up.railway.app/",
   withCredentials: true, // include cookies
 });
 
