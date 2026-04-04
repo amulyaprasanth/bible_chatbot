db336f6762ef14036e342795ae9ea7c3b1f8b305
Nadagani Amulya Prasanth <amulyaprasanth301@gmail.com>
2026-04-04 13:01:11 +0530

    switched dynamic link for backend

--- versechat/src/api/axios.ts
+++ versechat/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://bible-chatbot-backend.up.railway.app/",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
   withCredentials: true, // include cookies
 });
 
