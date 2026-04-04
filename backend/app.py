5a30b4f1801657b11e61ce0d9a9dbcd228c65983
Nadagani Amulya Prasanth <amulyaprasanth301@gmail.com>
2026-04-04 22:47:53 +0530

    fix cors error

--- backend/app.py
+++ backend/app.py
@@ -129,7 +129,8 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://biblechatbot.netlify.app"],
+    allow_origins=["http://localhost:5173",
+                   "https://versechatapp.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
