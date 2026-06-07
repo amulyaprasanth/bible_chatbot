b3792157e68b7e16a66417cb1e673e8edfc3ebd2
amulyaprasanth <amulyaprasanth301@gmail.com>
2026-06-07 15:58:47 +0530

    created eval structure, updated new frontend in cors

--- backend/app.py
+++ backend/app.py
@@ -118,7 +118,7 @@ app.include_router(router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                   "https://versechatapp.netlify.app"],
+                   "https://versechat.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
